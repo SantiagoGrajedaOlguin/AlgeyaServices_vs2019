215566f3f6f800cc7f9b08768193a79f0295fa13
SantiagoGrajedaOlguin <santiago.grajeda@gmail.com>
2021-08-09 12:20:13 -0500

    Servicio test

---
+++ AlgeyaInspeccionesServices2/AppInspeccionServicios/ISincronizacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AppInspeccionServicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ISincronizacion" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ISincronizacion
+    {
+        [OperationContract]
+        void Sincronizar(string DataCuerpo, string DataDetalle, string DataCalidades, string DataObservaciones, string DataObservacionesDetalle);
+    }
+}
---
+++ AlgeyaInspeccionesServices2/AppInspeccionServicios/Sincronizacion.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AppInspeccionServicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Sincronizacion" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Sincronizacion.svc o Sincronizacion.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Sincronizacion : ISincronizacion
+    {
+        public void Sincronizar(string DataCuerpo, string DataDetalle, string DataCalidades, string DataObservaciones, string DataObservacionesDetalle)
+        {
+            bool resultadoSrincronizacion = true;
+        }
+    }
+}
